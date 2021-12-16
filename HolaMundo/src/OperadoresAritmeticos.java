88c719d59362301fe9b6e4255ba17892c4854cbf
Santiago Vergara <santiago.vergaraq@perficient.com>
2021-12-16 13:49:11 -0500

    CJ-15 Corrección

--- HolaMundo/src/OperadoresAritmeticos.java
+++ HolaMundo/src/OperadoresAritmeticos.java
@@ -1,37 +1,37 @@
 import javax.swing.*;
 
 public class OperadoresAritmeticos {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int i = 5, j = 4, suma = i + j;
+        int i = 5, j = 4, suma = i + j;
 
-    System.out.println("suma = " + suma);
-    System.out.println("i + j = " + i + j);
+        System.out.println("suma = " + suma);
 
-    int resta = i - j;
-    System.out.println("resta = " + resta);
-    System.out.println("i - j = " + (i - j));
+        System.out.println("i + j = " + (i + j));
+        
+        int resta = i - j;
+        System.out.println("resta = " + resta);
+        System.out.println("(i - j) = " + (i - j));
 
-    int multi = i * j;
-    System.out.println("multiplicación = " + multi);
+        int multi = i * j;
+        System.out.println("multiplicación = " + multi);
+        
+        int div = i / j;
+        float div2 = (float) i / (float)j;
+        System.out.println("div = " + div);
+        System.out.println("div2 = " + div2);
+        
+        int resto = i % j;
+        System.out.println("resto = " + resto);
+        
+        resto = 8 % 5;
+        System.out.println("resto = " + resto);
 
-    int divi = i / j;
-    float div2 = (float) i / (float) j; 
-    System.out.println("división = " + divi);
-    System.out.println("div2 = " + div2);
-
-    int resto = i % j;
-    System.out.println("resto = " + resto);
-    
-    resto = 8 % 5;
-    System.out.println("resto = " + resto);
-
-    int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número"));
-    if (numero % 2 == 0) {
-      System.out.println("numero par = " + numero);
-    } else {
-      System.out.println("numero impar = " + numero);
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("ingrese un numero"));
+        if(numero % 2 == 0){
+            System.out.println("numero par = " + numero);
+        } else {
+            System.out.println("numero impar = " + numero);
+        }
     }
-
-  }
 }
